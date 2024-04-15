8d3fc37f86f5f2486cad9e6230343ccff2756c04
Santiago1470 <st147086@gmail.com>
2024-04-15 16:17:51 -0500

    Se agregó la autenticación con encriptación y la generación de token.

--- src/routers/animal.js
+++ src/routers/animal.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router(); //manejador de rutas de express
 const animalSchema = require("../models/animal");
+const verifyToken = require('./validate_token');
 //Nuevo animal
 router.post("/animals", (req, res) => {
     const animal = animalSchema(req.body);
@@ -10,7 +11,7 @@ router.post("/animals", (req, res) => {
         .catch((error) => res.json({ message: error }));
 });
 
-router.get("/animals", (req, res) => {
+router.get("/animals", verifyToken, (req, res) => {
     animalSchema.find()
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }));
